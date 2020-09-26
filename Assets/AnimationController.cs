bb9b83ca50a64d3cb16dbfe9b0ab07680ac4896e
dhiraj7894 <dhiraj7894@gmail.com>
2020-09-27 00:20:35 +0530

    jkghuyhj

--- Assets/AnimationController.cs
+++ Assets/AnimationController.cs
@@ -6,29 +6,47 @@ public class AnimationController : MonoBehaviour
 {
     public Animator[] player;
 
+    private void Start()
+    {
+            player[3].enabled = false;
+            player[4].enabled = false;
+            player[5].enabled = false;
+    }
     public void idlePlayer()
     {
-        player[0].SetBool("idle", true);
+            player[0].SetBool("idle", true);
     }
 
     public void grabIn()
     {
-        player[1].SetBool("Grab", true);
+            player[1].SetBool("Grab", true);
     }
 
     public void grabOut()
     {
-        player[1].SetBool("Grab", false);
+            player[1].SetBool("Grab", false);
     }
 
     public void cutIn()
     {
         //player[2].SetBool("CutLoop",true);
-        player[2].enabled = true;
+            player[2].enabled = true;
     }
     public void cutOut()
     {
         //player[2].SetBool("CutLoop", false);
-        player[2].enabled = false;
+            player[2].enabled = false;
+    }
+    public void leftFold()
+    {
+            player[3].enabled = true;
+    }
+    public void rightFold()
+    {
+            player[4].enabled = true;
+    }
+    public void downFold()
+    {
+            player[5].enabled = true;
     }
 }
---
+++ Assets/UpdateScript/scene1_Animation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class scene1_Animation : MonoBehaviour
+{
+    public Animator[] player;
+    public TextMeshProUGUI text;
+    public Slider sld;
+
+    private void Start()
+    {
+        player[0].SetBool("walk", true);
+    }
+
+    private void Update()
+    {
+        text.text = sld.value + "/2".ToString();
+    }
+    public void idlePlayer()
+    {
+        player[0].SetBool("idle", true);
+    }
+
+    public void grabIn()
+    {
+        player[1].SetBool("Grab", true);
+    }
+
+    public void grabOut()
+    {
+        player[1].SetBool("Grab", false);
+    }
+}
